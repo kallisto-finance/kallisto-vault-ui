152d5d2026da2b0c462dc8b9aba7f5e7b42308aa
Jason Jacobs <nerfnerd@gmail.com>
2022-07-13 14:57:51 -0700

    this is a server side header don't put into client

--- utils/axios.ts
+++ utils/axios.ts
@@ -4,11 +4,7 @@ const apiBase = process.env.KALLISTO_VAULT_API_BASE;
 
 const axiosClient = () => {
   const client = axios.create();
-
-  client.defaults.headers.common = {
-    "Access-Control-Allow-Origin": "*",
-  };
-
+  
   return client;
 };
 
@@ -18,6 +14,6 @@ export const fetchCurveVaultValues = async () => {
     console.log(res.data);
     return res.data;
   }
-  
+
   return null;
 }
